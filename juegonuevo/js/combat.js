072eee2b39b1af5575ad352389969032688070d1
NazarenaAranda <Nazarenaaranda15@gmail.com>
2022-12-17 17:45:12 -0300

    seleccion aleatoria de nuestro enemigo y cambio del httml usando js

--- juegonuevo/js/combat.js
+++ juegonuevo/js/combat.js
@@ -4,40 +4,70 @@ function iniciarJuego() {
 // Escucha cuando se hace click al elemento que llamamos en la variable botonMascotaJugador anteriormente, y al escuchar esto tiene como segundo parametro una funcion en la cual luego escribiremos lo que queremos que pase luego de que se cumple la accion del "click"
 botonMascotaJugador.addEventListener("click", seleccionarMascotaJugador)
 }
-function seleccionarMascotaJugador() {
+function seleccionarMascotaJugador()
+{
     let inputFenixita = document.getElementById("fenixita")
     let inputDelfipower = document.getElementById("delfipower")
     let inputTopotaque = document.getElementById("topotaque")
     let inputOxidianota = document.getElementById("oxidianota")
     let inputTortuninja = document.getElementById("tortuninja")
     let inputDragonburri = document.getElementById("dragonburri")
+    let spanMascotaJugador = document.getElementById("mascota-jugador")
     if (inputFenixita.checked == true)
     {
-        alert("Seleccionaste a Fenixita")
+        // Modifica el Html desde js manipulando el DOM
+        spanMascotaJugador.innerHTML = "Fenixita "
     }
     else if (inputDelfipower.checked == true)
     {
-        alert("Seleccionaste a Delfipower")
+        spanMascotaJugador.innerHTML = "Delfipower "
     }
     else if (inputTopotaque.checked == true)
     {
-        alert("Seleccionaste a Topotaque")
+        spanMascotaJugador.innerHTML = "Topotaque "
     }
     else if (inputOxidianota.checked == true)
     {
-        alert("Seleccionaste a Oxidianota")
+        spanMascotaJugador.innerHTML = "Oxidianota "
     }
     else if (inputTortuninja.checked == true)
     {
-        alert("Seleccionaste a Tortuninja")
+        spanMascotaJugador.innerHTML = "Tortuninja "
     }
     else if (inputDragonburri.checked == true)
     {
-        alert("Seleccionaste a Dragonburri")
+        spanMascotaJugador.innerHTML = "Dragonburri "
     }
     else {
         alert("Selecciona alguna mascota")
     }
+    seleccionarMascotaEnemigo() 
 }
+function seleccionarMascotaEnemigo() {
+    let enemigoAleatorio = aleatorio(1, 6)
+    let spanMascotaEnemigo = document.getElementById("mascota-enemigo")
+    if (enemigoAleatorio == 1) {
+        spanMascotaEnemigo.innerHTML = "Fenixita "
+    }
+    else if (enemigoAleatorio == 2) {
+        spanMascotaEnemigo.innerHTML = "Delfipower "
+    }
+    else if (enemigoAleatorio == 3) {
+        spanMascotaEnemigo.innerHTML = "Topotaque "
+    }
+    else if (enemigoAleatorio == 4) {
+        spanMascotaEnemigo.innerHTML = "Oxidianota "
+    }
+    else if (enemigoAleatorio == 5) {
+        spanMascotaEnemigo.innerHTML = "Tortuninja "
+    }
+    else if (enemigoAleatorio == 6) {
+        spanMascotaEnemigo.innerHTML = "Dragonburri "
+    }
+}
+function aleatorio(min, max) {
+        return Math.floor(Math.random() * (max - min + 1) + min)
+    }
+
 // llama a la funcion iniciarJuego cuando carga todo el html
 window.addEventListener("load", iniciarJuego)
